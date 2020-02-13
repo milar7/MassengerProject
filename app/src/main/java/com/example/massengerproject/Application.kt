3adcd8638880d43ba587172eb199320b5dd3deed
milar7 <miladir@yahoo.com>
2020-02-13 19:16:43 +0330

    test commit2

--- app/src/main/java/com/example/massengerproject/Application.kt
+++ app/src/main/java/com/example/massengerproject/Application.kt
@@ -19,20 +19,21 @@ import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
-class Application: Application(),KodeinAware {
-    override val kodein= Kodein.lazy {
+class Application : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
         import(androidXModule(this@Application))
         bind() from singleton { NetWorkConnentionInterceptor(instance()) }
-        bind() from  singleton { MyApi(instance()) }
-        bind() from  singleton { AppDatabase(instance()) }
-        bind() from  singleton { PreferenceProvider(instance()) }
-        bind() from  singleton { UserRepository(instance(),instance()) }
-        bind() from  singleton { QuoteRepository(instance(),instance(),instance()) }
-        bind() from  provider {AuthViewModelFactory(instance())  }
-        bind() from  provider {ProfileViewModelFactory(instance())  }
-        bind() from  provider {QuotesViewModelFactory(instance())  }
+        bind() from singleton { MyApi(instance()) }
+        bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { PreferenceProvider(instance()) }
+        bind() from singleton { UserRepository(instance(), instance()) }
+        bind() from singleton { QuoteRepository(instance(), instance(), instance()) }
+        bind() from provider { AuthViewModelFactory(instance()) }
+        bind() from provider { ProfileViewModelFactory(instance()) }
+        bind() from provider { QuotesViewModelFactory(instance()) }
 
 //test
+        //first spring
 
     }
 }
